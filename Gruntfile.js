0c607046cb48c4562bc616b4916aba0328901140
eafelix <eafelix@outlook.com>
2014-12-27 16:46:47 -0300

    add

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,14 +11,20 @@ module.exports = function(grunt) {
                 src: '/src/<%= pkg.name %>.js',
                 dest: 'build/<%= pkg.name %>.min.js'
             }
+        },
+        jshint : {
+            src:['*.js','./public/scripts/*.js']
         }
     });
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Load the plugin that provides the "jshint" task.
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
     // Default task(s).
-    grunt.registerTask('default', ['uglify','jshint']);
+    grunt.registerTask('default', ['jshint','uglify']);
+    grunt.registerTask('test', ['uglify','jshint']);
 
 };
